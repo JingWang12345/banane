648df732ea9395ca39effd0b6a0be8a73595eb8c
JingWang <wangjing061993@gmail.com>
2023-09-23 18:46:43 +0200

    updated

--- sketch.js
+++ sketch.js
@@ -1,75 +1,28 @@
-let streams = [];
-let speeds = [];
+let c=document.getElementById('p5-canvas');
+let ctx=c.getContext("2d");
 
-let NUM_STREAMS = 30;
-let MIN_STREAK = 4;
-let GAP_MAX = 20
-let ts = Array(NUM_STREAMS).fill(0);
+let colors = ["#233D4D", "#FE7F2D", "#FCCA46", "#A1C181", "#619B8A"];
+let particles = [];
+let amount = 40;
 
 function setup() {
-	createCanvas(200, 200, document.getElementById('p5-canvas'));
-	for (let i = 0; i < NUM_STREAMS; i++){
-		
-		let s = round(random(1, 5));
-		speeds.push(s);
-		
-		let acc = [];
-		let randCoeff = random(0.5, 1);
-		for (let x = 0; x < width; x+=MIN_STREAK){
-			if (random() > randCoeff) {
-				let gapLen = random(MIN_STREAK, GAP_MAX);
-								
-				for (let k = 0; k < gapLen; k++){
-					acc.push(false);
-				}
-				x += gapLen;
-			}
-			else {
-				for (let k = 0; k < MIN_STREAK; k++) {
-					acc.push(true);
-				}
-			}
-		}
-		streams.push(acc);
-	}
+  createCanvas(300, 300);
+  noStroke();
+  frameRate(20);
+  let p = new Particle(random(width), random(height), random(colors));
+  particles.push(p);
 }
 
 function draw() {
-	background(250);
-	strokeWeight(6.5);
-	strokeCap(SQUARE);
-	noFill();
-	let i = 0
-	
-	for (let y = 0; y < height - (height/NUM_STREAMS); y += height/NUM_STREAMS) {
-		let begun = true;
-		beginShape();
-		let stream = streams[i];
-		
-		let speedMod = map(mouseX, 0, width, 1, 10);
-		t = ts[i];
-		ts[i] += speeds[i] * round(speedMod);
-		
-		let shift = y/200 + speedMod;
-		let freq = map(mouseY, 0, height, height*0.25, height*0.05);
-		let getY = (xVal) => sin(xVal/freq - frameCount/3000 + shift) * height/NUM_STREAMS + y;
-		
-		for (let x = 0; x < width; x+=1){
-			let val = stream[(x + t) % width];
-			if (val && begun) {
-				vertex(x, getY(x));
-			} else if (val && !begun) {
-				begun = true;
-				beginShape();
-				vertex(x, getY(x));
-			} else if (!val && begun){
-				endShape();
-				begun = false;
-				
-				blendMode(BLEND);
-			}
-		}
-		endShape();
-		i += 1;
-	}
+  background("#282846");
+  for (let i = 0; i < particles.length; i++) {
+    if (particles[i].lv < 0) {particles.splice(i, 1);}
+    particles[i].update();
+    particles[i].show();
+  }
+  if (particles.length < amount) {
+    let p = new Particle(random(width), random(height), random(colors));
+    particles.push(p);
+  }
 }
+
